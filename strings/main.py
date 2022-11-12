c5c0b77f627ff55e478d2ba13122c214ed15256e
Dana Devops <danadevops@gmail.com>
2022-11-12 17:13:33 +0100

    edited

--- strings/main.py
+++ strings/main.py
@@ -12,21 +12,17 @@ scorers=scorer_1 + " " + str(goal_0) + "," + " " + scorer_2 + " " + str(goal_1)
 report= f"{scorer_1} scored in the {goal_0}nd minute\n{scorer_2} scored in the {goal_1}th minute"
 print(len(scorer_1))
 
-player=scorer_1
-first_name=player[:4]
-find_first_name=player.find("Ruud")
+player="Er Koema"
+find_first_name=player.find(" ")
+first_name=player[:find_first_name]
 print(first_name)
 
+find_last_name=player.find(" ")
+last_name_len=len(player[find_last_name:-1])
+print(last_name_len)
 
-find_last_name=scorer_1.find("Gullit")
-name_lenght=len(player)
-slice_last_name= scorer_1[5:11]
-last_name_len=len(slice_last_name)
-last_name=scorer_1[find_last_name:name_lenght]
-print(last_name)
-
-
-name_short=f"{first_name[0]}. {last_name}"
+last_name=player[find_first_name:]
+name_short=f"{first_name[0]}.{last_name}"
 print(name_short)
 
 length_first_name=len(first_name)
